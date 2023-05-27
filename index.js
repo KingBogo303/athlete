0b31d97787e6a59294e4d0b9319b8e3710df75d2
Bogo <josephogbo111@gmail.com>
2023-05-27 15:59:26 -0700

    implemented toast

---
+++ index.js
@@ -0,0 +1,7 @@
+// var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+// var toastList = toastElList.map(function (toastEl) {
+//   return new bootstrap.Toast(toastEl, option)
+// })
+
+const toast = new bootstrap.Toast('.toast')
+toast.show()
